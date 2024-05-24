f003fdf5aea0ed158fee3f9efc99b0892b462bb9
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2024-05-24 14:34:44 -0400

    Finished read_stats workflow. Added dir check to sort_dirs.py prior to moving.

--- scripts/read_stats/summary.sh
+++ scripts/read_stats/summary.sh
@@ -2,50 +2,11 @@
 
 set -euo pipefail
 
-while getopts 'i:o:r:e:p' flag; do
-  case "${flag}" in
-    i) input_dir=${OPTARG} ;;
-    o) output_dir=${OPTARG} ;;
-    r) regex_read_path=${OPTARG} ;;
-    e) output_plot_ext=${OPTARG} ;;
-    p) processes=${OPTARG} ;;
-    *) usage; exit 1 ;;
-  esac
-done
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate read_stats
 
-run_name=$(basename $input_dir)
-output_dir=${output_dir:-"${input_dir}/summary"}
-output_plot_ext=${output_plot_ext:-"png"}
-processes=${processes:-12}
-regex_read_path=${regex_read_path:-"${input_dir}/.*?/.*?/bam_pass/.*bam"}
+wd=$(dirname $0)
 
-mkdir -p "${output_dir}/read_len"
+snakemake -p -d $wd -s $wd/Snakefile --configfile $wd/config.yaml $@
 
-dorado_read_len_summary() {
-    local bam_file=$1
-    local out_dir=$2
-    local read_name=$(basename ${bam_file} .bam)
-    local summary_file="${out_dir}/read_len/${read_name}.tsv"
-
-    /project/logsdon_shared/tools/dorado-0.6.1-linux-x64/bin/dorado summary ${bam_file} | \
-    awk -v OFS='\t' 'NR >1 {
-        if (NF == 12) {
-            print $2, $10, $1
-        } else {
-            print $1, $9, "None"
-        }
-    }' > ${summary_file}
-}
-
-export -f dorado_read_len_summary
-
-find ${input_dir} -regex "${regex_read_path}" | \
-    xargs -P ${processes} -I {} bash -c "dorado_read_len_summary {} ${output_dir}"
-
-cat ${output_dir}/read_len/*.tsv > ${output_dir}/read_len/all_reads_len.tsv
-
-python /project/logsdon_shared/tools/ont_stats \
-    --fais "${run_name}=${output_dir}/all_reads_len.tsv" \
-    --plot_dir ${output_dir}/plot \
-    --plot_ext ${output_plot_ext} \
-    -t > ${output_dir}/summary.tsv
+conda deactivate
--- scripts/sort_dirs/sort_dirs.py
+++ scripts/sort_dirs/sort_dirs.py
@@ -1,6 +1,7 @@
 import re
 import os
 import sys
+import time
 import shutil
 import argparse
 import logging
@@ -9,9 +10,10 @@ from enum import StrEnum
 logging.basicConfig(
     stream=sys.stderr,
     level=logging.DEBUG,
-    format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
-    datefmt='%H:%M:%S'
+    format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
+    datefmt="%H:%M:%S",
 )
+
 
 class LongReadArchiveSubDir(StrEnum):
     CLINICAL = "clin"
@@ -25,10 +27,35 @@ RGX_DIR_PATTERN = re.compile(
     r"(?P<year>\d{4})_(?P<month>\d{2})_(?P<day>\d{2})_(?P<category>.*?)_(?P<sample>.*?)_(?P<seq_kit>ULK.*?|LSK.*?)_(?P<library_kit>.*?)$"
 )
 
+
+def get_size(start_path: str):
+    total_size = 0
+    for dirpath, _, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
 def main():
-    ap = argparse.ArgumentParser(description="Script to sort sequencing run directories.")
-    ap.add_argument("-i", "--input_dir", help="Input directory of sequencing run subdirs.", required=True)
-    ap.add_argument("-o", "--output_dir", default="/project/logsdon_shared/long_read_archive", help="Output directory to move dirs.")
+    ap = argparse.ArgumentParser(
+        description="Script to sort sequencing run directories."
+    )
+    ap.add_argument(
+        "-i",
+        "--input_dir",
+        help="Input directory of sequencing run subdirs.",
+        required=True,
+    )
+    ap.add_argument(
+        "-o",
+        "--output_dir",
+        default="/project/logsdon_shared/long_read_archive",
+        help="Output directory to move dirs.",
+    )
     args = ap.parse_args()
 
     os.makedirs(args.output_dir, exist_ok=True)
@@ -43,19 +70,29 @@ def main():
             try:
                 sorted_dir = LongReadArchiveSubDir(mtch["category"])
             except ValueError:
-                logging.error(f"Invalid category {mtch['category']} for directory, {og_dir_path}")
+                logging.error(
+                    f"Invalid category {mtch['category']} for directory, {og_dir_path}"
+                )
                 continue
-            
+
+            # Check that directory is not changing in size.
+            og_size = get_size(og_dir_path)
+            time.sleep(2)
+            new_size = get_size(og_dir_path)
+            if og_size != new_size:
+                logging.info(f"Directory {og_dir_path} is changing in size. Skipping.")
+                continue
+
             new_dir_path = os.path.join(args.output_dir, sorted_dir)
             os.makedirs(sorted_dir, exist_ok=True)
 
             shutil.move(og_dir_path, new_dir_path)
-            
+
             logging.info(f"Moved {og_dir_path} to {new_dir_path}.")
             total_dirs_moved += 1
 
     logging.info(f"Moved {total_dirs_moved} directories.")
-    
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
