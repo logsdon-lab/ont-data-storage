08ec676c3c6b577a80622bce63c3212bc2c387e1
koisland <koshima789@gmail.com>
2024-06-05 09:42:30 -0400

    Also kill dorado to stop basecalling.

--- scripts/basecall/stop_basecall.sh
+++ scripts/basecall/stop_basecall.sh
@@ -4,5 +4,5 @@ day_of_week=$(date +%u)
 if [ $day_of_week -eq 6 ] || [ $day_of_week -eq 7 ]; then
     echo "Not stopping any basecalling because it's the weekend."
 else
-    (pkill -f "snakemake" && echo "Ended basecalling." ) || echo "No basecalling process to end."
+    (pkill -f "snakemake" && pkill -f "dorado" && echo "Ended basecalling." ) || echo "No basecalling process to end."
 fi
