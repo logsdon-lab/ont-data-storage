eb975c1b67bcbd8d9bd4bdf87a2528541e1897c2
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2024-06-29 05:15:10 -0400

    Fix wildcard expansion. Change command to not capture output.

--- scripts/read_stats/summary.sh
+++ scripts/read_stats/summary.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 wd=$(dirname $0)
 
-mamba run -n read_stats snakemake -p -d $wd -s $wd/Snakefile --configfile $wd/config.yaml $@
+mamba run --no-capture-output -n read_stats snakemake -p -d $wd -s $wd/Snakefile --configfile $wd/config.yaml $@
