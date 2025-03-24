7aa1bccea230fd7b75963965a7f76eae396ea57d
Keith Oshima <koshima789@gmail.com>
2025-03-24 10:04:47 -0400

    Add option to transfer files if unbasecalled.

--- scripts/sync_data/sync_data.sh
+++ scripts/sync_data/sync_data.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 usage() {
-  echo "Usage: $0 [-u user_host] [-i input_dir] [-o output_dir] [-r regex_data_dir] [-nh]"
+  echo "Usage: $0 [-u user_host] [-i input_dir] [-o output_dir] [-r regex_data_dir] [-nah]"
   echo "Sync files via rsync."
   echo ""
   echo "Arguments:"
@@ -12,19 +12,21 @@ usage() {
   echo "    -u      Output directory"
   echo "    -r      Regular expression pattern to sync from input directory."
   echo "    -n      Dry-run"
+  echo "    -a      Transfer ignoring basecalling checkpoint file."
   echo "    -h      Print help."
   echo ""
   echo "Example:"
   echo "./scripts/sync_data.sh -u "s_prom@sarlacc.pmacs.upenn.edu" -i '/data' -o '/project/logsdon_shared/long_read_archive/unsorted' -r '/data/20[2-9][0-9]{5}.*'"
 }
 
-while getopts 'u:i:o:r:hn' flag; do
+while getopts 'u:i:o:r:hna' flag; do
   case "${flag}" in
     u) host=${OPTARG} ;;
     i) input_dir=${OPTARG} ;;
     o) output_dir=${OPTARG} ;;
     r) regex_data_dir=${OPTARG} ;;
     n) dry_run="true" ;;
+    a) allow_unbasecalled="true" ;;
     h) usage; exit 0 ;;
     *) usage; exit 1 ;;
   esac
@@ -36,6 +38,7 @@ input_dir=${input_dir:-'/data'}
 output_dir=${output_dir:-'/project/logsdon_shared/long_read_archive/unsorted'}
 regex_data_dir=${regex_data_dir:-"${input_dir}/20[2-9][0-9]{5}.*"}
 dry_run=${dry_run:-'false'}
+allow_unbasecalled=${allow_unbasecalled:-'false'}
 
 # Find data directories matching regex pattern.
 # We restrict depth to avoid finding subdirs, etc.
@@ -47,7 +50,7 @@ for dir in ${data_dirs}; do
   # Number of dirs should match number of dirs with basecalling.done
   num_dirs=$(find "${dir}" -mindepth 1 -maxdepth 1 -type d -printf '.' | wc -c)
   num_basecalled_dirs=$(find "${dir}" -wholename "*/pod5/basecalling/basecalling.done" -printf '.' | wc -c)
-  if [ "${num_basecalled_dirs}" -ne "${num_dirs}" ]; then
+  if [ "${allow_unbasecalled}" == "false" ] && [ "${num_basecalled_dirs}" -ne "${num_dirs}" ]; then
     echo "${dir} not basecalled. Skipping..."
     continue
   fi
